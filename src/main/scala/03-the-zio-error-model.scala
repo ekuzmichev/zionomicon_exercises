bc2f0c31798d10a19bf655734767fdcc1e5a0102
ekuzmichev <evgenii.e.kuzmichev@gmail.com>
2026-02-09 22:11:11 +0300

    Solve Exercise 3.10

--- src/main/scala/03-the-zio-error-model.scala
+++ src/main/scala/03-the-zio-error-model.scala
@@ -144,4 +144,5 @@ object TheZIOErrorModel:
     def catchAllCause[R, E1, E2, A](
         zio: ZIO[R, E1, A],
         handler: Cause[E1] => ZIO[R, E2, A]
-    ): ZIO[R, E2, A] = ???
+    ): ZIO[R, E2, A] =
+      zio.foldCauseZIO(cause => handler(cause), a => ZIO.succeed(a))
